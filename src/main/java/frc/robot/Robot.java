3c902b9095b700a8c00ed02e097a8c48a5aa9d4c
Karl Fleischmann <karl_fleischmann@yahoo.com>
2018-12-05 16:08:44 -0500

    Add buttons for pickup and thrower

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.Joystick.ButtonType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
@@ -21,14 +22,23 @@ public class Robot extends IterativeRobot {
   private DifferentialDrive m_myRobot;
   private Victor m_leftDriveMotor;
   private Victor m_rightDriveMotor;
+
   private Joystick m_joystick;
   
+  private Victor m_pickupMotor;
+  private Victor m_throwingMotor;
+  
+
   @Override
   public void robotInit() {
     //Drivetrain
     m_leftDriveMotor = new Victor(0);
     m_rightDriveMotor = new Victor(1);
     m_myRobot = new DifferentialDrive(m_leftDriveMotor, m_rightDriveMotor);
+
+    //Extensions
+    m_pickupMotor = new Victor(2);
+    m_throwingMotor = new Victor(3);
 
     //Joysticks
     m_joystick = new Joystick(0);
@@ -37,5 +47,39 @@ public class Robot extends IterativeRobot {
   @Override
   public void teleopPeriodic() {
     m_myRobot.tankDrive(m_joystick.getRawAxis(0), m_joystick.getRawAxis(1));
+
+    //Operate the pickup
+    if (m_joystick.getRawButton(0))
+    { 
+      //Forward
+      m_pickupMotor.set(1); 
+    }
+    else if (m_joystick.getRawButton(1))
+    { 
+      //Reverse
+      m_pickupMotor.set(-1); 
+    }
+    else
+    { 
+      //Stop
+      m_pickupMotor.set(0); 
+    }
+
+    //Operate the Thrower
+    if (m_joystick.getRawButton(2))
+    { 
+      //Forward
+      m_throwingMotor.set(1); 
+    }
+    else if (m_joystick.getRawButton(3))
+    { 
+      //Reverse
+      m_throwingMotor.set(-1); 
+    }
+    else
+    { 
+      //Stop
+      m_throwingMotor.set(0); 
+    }
   }
 }
